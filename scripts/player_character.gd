5535819f7ff86faef969273216ea6fbed36b5b2e
Sriram Ramesh <sriram@vananam.com>
2026-01-28 19:29:23 +0530

    added collision function to player character
    The player character will disapear on collision using the hide()
    function which is a function of the Area2D node

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+signal hit
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
@@ -39,3 +39,10 @@ func _process(delta: float) -> void:
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(_body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
